125b69534f0c80459e2539dc92e50d6d455250fa
IvanLuciano <ivan09449@gmail.com>
2022-02-22 14:54:49 -0300

    funcion

--- funcion.js
+++ funcion.js
@@ -2,11 +2,28 @@ $(document).ready(function() {
 
   $(".contenedor").scroll(function () {
     var y = $(this).scrollTop();
+    console.log(y);
     if (y >= 520 & y <= 3240) {
       $(".caractImgFixed").fadeIn(250);
     } else {
       $(".caractImgFixed").fadeOut(50);
     }
     console.log(y);
+
+    //Habilitar causas
+
+    if(y > 4500){
+
+      $(".causas").css( "display", "flex" );
+      $(".contenedor").hide()
+    }
+
+
   });
+
+
+
+
+
+
 });
